e32e2129beb36b799794a5019a5780f1fc9b5439
belleharvey <belle.harvey@students.mq.edu.au>
2025-06-11 15:23:12 +1000

    added kenney tilemaps

---
+++ Assets/Tiled/Industrial.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Industrial" tilewidth="21" tileheight="21" tilecount="448" columns="28">
+ <image source="../kenney_platformer-pack-industrial/Tilesheet/platformerPack_industrial_tilesheet3.png" width="588" height="336"/>
+</tileset>
